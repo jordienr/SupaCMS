46dff1b98c30415a9e60b214ab96f1b6d5e29063
Jordi <jordi.err@gmail.com>
2021-11-08 17:00:59 +0100

    add some style improvements

--- src/App.vue
+++ src/App.vue
@@ -16,15 +16,19 @@ export default {
 </script>
 
 <template>
-  <div class="flex min-h-screen">
-    <aside class="pt-6 px-4 w-64 bg-gray-800 text-white">
-      <router-link to="/" class="p-2 text-xl font-medium">
+<div class="container mx-auto">
+  <div class="flex">
+    <aside class="py-4 w-64 min-w-max m-4 border rounded-md bg-white">
+      <router-link to="/" class="p-2 px-6 text-xl font-medium">
         {{ appName }}
       </router-link>
-      <div class="flex flex-col gap-2 mt-6">
+      <div class="flex flex-col mt-6">
         <router-link
           v-for="t in tables"
-          class="px-6 py-2 rounded-md hover:bg-gray-900"
+          class="px-6 py-2 text-gray-500 hover:text-gray-900 hover:bg-gray-50"
+          :class="{
+            'bg-gray-100 text-gray-900 font-medium border-l-2 border-gray-800': this.$route.path.includes(t.name)
+          }"
           :key="t.name"
           :to="'t/' + t.name"
         >
@@ -36,6 +40,11 @@ export default {
       <router-view></router-view>
     </main>
   </div>
+</div>
 </template>
 
-<style></style>
+<style>
+body {
+  background-color: theme('colors.gray.100')
+}
+</style>
--- src/components/TableView.vue
+++ src/components/TableView.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="bg-gray-100 p-4 min-h-full block">
+  <div class="bg-gray-100 p-4 min-h-full w-full block">
     <div class="flex justify-between items-center">
       <h1 class="text-xl font-medium">{{ tableConfig.label }}</h1>
-      <button class="px-4 py-2 border bg-white rounded-md">Add row</button>
+      <button class="px-4 py-2 border bg-white rounded-md" @click="addRow">Add row</button>
     </div>
     <pre v-if="loading">loading...</pre>
     <pre v-if="error" class="bg-red-50">{{ error }}</pre>
     <div
       v-if="!loading"
-      class="mt-4 shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"
+      class="mt-4 overflow-hidden border border-gray-200 rounded-md"
     >
       <table class="min-w-full divide-y divide-gray-200">
         <thead class="bg-gray-50">
@@ -22,6 +22,7 @@
                 text-gray-500
                 uppercase
                 tracking-wider
+                w-52
               "
               v-for="cl in colLabels"
               :key="cl"
@@ -31,13 +32,19 @@
             <th></th>
           </tr>
         </thead>
-        <tr class="bg-white" v-for="(row, ind) in tableData" :key="ind">
-          <td class="px-6 py-3" v-for="td in row" :key="td">{{ td }}</td>
-          <td class="">
-            <button class="font-medium text-blue-500 px-6">Edit</button>
+        <tr class="bg-white truncate max-w-sm" v-for="(row, ind) in tableData" :key="ind">
+          <td class="px-6 py-3 max-w-md overflow-x-hidden overflow-ellipsis" v-for="td in row" :key="td">{{ td }}</td>
+          <td class="text-right">
+            <button class="font-medium text-blue-500 px-6 py-2 rounded-md transition-colors hover:bg-blue-50">Edit</button>
+            <button class="font-medium text-red-500 px-6 py-2 rounded-md transition-colors hover:bg-red-50">Delete</button>
           </td>
         </tr>
       </table>
+    </div>
+  </div>
+  <div v-if="showAddRow" @click.stop="showAddRow = false" class="bg-gray-800 fixed top-0 left-0 z-index-1 w-full bg-opacity-70 h-screen flex justify-center items-end">
+    <div @click.stop class="bg-white container mx-auto mb-8 p-6 rounded-md">
+      showAddRow
     </div>
   </div>
 </template>
@@ -51,6 +58,7 @@ export default {
       loading: true,
       tableData: [],
       error: undefined,
+      showAddRow: false
     };
   },
   computed: {
@@ -69,6 +77,8 @@ export default {
     colSelect() {
       return this.cols.map((c) => c.name).join();
     },
+    addRowForm() {
+    }
   },
   methods: {
     async fetchTableData() {
@@ -80,6 +90,10 @@ export default {
       this.error = error;
       this.loading = false;
     },
+    async addRow() {
+      console.log('addRow')
+      this.showAddRow = true
+    }
   },
   mounted() {
     this.fetchTableData();
