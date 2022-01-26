da2567db9a610c3d7d90a22f1af25fea73be5a7d
Jordi <jordi.err@gmail.com>
2022-01-26 20:03:37 +0100

    add website scaffold

---
+++ website/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <header
+    class="px-4 min-h-screen bg-slate-100 text-center flex items-center flex-col justify-center"
+  >
+    <h1 class="font-medium text-2xl">SupaCMS</h1>
+    <p class="text-lg mt-4">Get a CMS for your Supabase project in minutes</p>
+
+    <p>test</p>
+
+    <pre></pre>
+  </header>
+</template>
---
+++ website/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
